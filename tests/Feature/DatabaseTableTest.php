e11f1f875b1d5611a18d6030bdbce4222db6581c
utifmd <utifmd@gmail.com>
2023-10-30 15:13:35 +0700

    database connection test

---
+++ tests/Feature/DatabaseTableTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class DatabaseTableTest extends TestCase
+{
+    public function testGetConnection()
+    {
+        $connection = DB::connection()->getPdo();
+        self::assertNotNull($connection);
+    }
+
+    public function testDatabaseSingleton()
+    {
+        $connection1 = DB::connection()->getPdo();
+        $connection2 = DB::connection()->getPdo();
+
+        self::assertSame($connection1, $connection2);
+    }
+}
